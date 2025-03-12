dabe4adabf2cbc361c0e23ee8b59f3b4651047b5
Henry Nguyen <knguyen@icstars.org>
2025-03-11 22:52:19 -0500

    final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Theme>
-      <App />
-    </Theme>
-  </React.StrictMode>
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
